68d6a43042a631201c9bc49c9dd93115611bc93b
AnPhamHong <an.ph410@gmail.com>
2020-10-31 04:05:12 +0700

    My-first-code-saleappCNPM
    Code thực hành 28/10/2020

---
+++ saleapp/utils.py
@@ -0,0 +1,15 @@
+#cung cap dich vu tien ich cho cac lop khac
+import json
+
+
+def read_data(path = 'data/categories.json'):
+    with open(path, encoding='utf-8') as f:
+        return json.load(f)
+
+
+
+def get_product_by_id(product_id):
+    products = read_data('data/products.json')
+    for p in products:
+        if p["id"] == product_id:
+            return p
